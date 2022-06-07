a23498416c982d5af1ea3eaea01eec206dab1394
Justudude321 <105064710+Justudude321@users.noreply.github.com>
2022-06-07 00:26:07 -0400

    maybe fixed

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4481,7 +4481,7 @@ class PlayState extends MusicBeatState
 				switch(achievementName)
 				{
 					case 'week1_nomiss' | 'week2_nomiss' | 'week3_nomiss' | 'week4_nomiss' | 'week5_nomiss' | 'week6_nomiss' | 'week7_nomiss':
-						if(isStoryMode && CoolUtil.difficultyString() == 'HARD' && storyPlaylist.length <= 1 && !changedDifficulty && !usedPractice)
+						if(isStoryMode && campaignMisses + songMisses < 1 && CoolUtil.difficultyString() == 'HARD' && storyPlaylist.length <= 1 && !changedDifficulty && !usedPractice)
 						{
 							var weekName:String = WeekData.getWeekFileName();
 							switch(weekName) //I know this is a lot of duplicated code, but it's easier readable and you can add weeks with different names than the achievement tag
@@ -4547,7 +4547,7 @@ class PlayState extends MusicBeatState
 						if(deathCounter > 0 && curStage == 'stage') {
 							unlock = true;
 						}
-
+					}
 				if(unlock) {
 					Achievements.unlockAchievement(achievementName);
 					return achievementName;
