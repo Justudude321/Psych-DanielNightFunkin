83916b15976585a52fece89fadcdd14cbc482d50
Justudude321 <105064710+Justudude321@users.noreply.github.com>
2022-08-07 18:20:33 -0400

    updated

--- assets/preload/data/joyous/healthdrain.lua
+++
@@ -1,7 +0,0 @@
-function opponentNoteHit(id, direction, noteType, isSustainNote)
-
-	if dadName == 'carol' then	
-	setProperty('health', getProperty('health') - 0.006);
-	end
-	
-end
--- assets/preload/data/kagayaku-kassetto/healthdrain.lua
+++
@@ -1,7 +0,0 @@
-function opponentNoteHit(id, direction, noteType, isSustainNote)
-
-	if dadName == 'CG' then	
-	setProperty('health', getProperty('health') - 0.008);
-	end
-	
-end
