d4ce82227648a5126099eeb9a52696a0649e476d
Justudude321 <105064710+Justudude321@users.noreply.github.com>
2022-08-09 18:36:02 -0400

    Fixes, additions, and reorganizing for now

---
+++ assets/preload/data/atrocity/defaultCamZoom.lua
@@ -0,0 +1,26 @@
+function onEvent(name,value1,value2)
+    if name == 'defaultCamZoom' then
+        setProperty('defaultCamZoom', value1)
+    end
+end
+
+function onStepHit()
+	if curStep == 128 then
+		doTweenAlpha('in1', 'camHUD', 1, 1)
+		setProperty('cameraSpeed', 1)
+	end
+	if curStep == 1536 then
+		if flashingLights == true then
+			cameraFlash('camHUD', 'FFFFFF', 1)
+		end
+	end
+	if curStep == 1584 then
+		setProperty('cameraSpeed', 1)
+	end
+	if curStep == 1856 then
+		setProperty('cameraSpeed', 100)
+	end
+	if curStep == 1921 then
+		setProperty('boyfriendGroup.alpha', false)
+	end
+end
