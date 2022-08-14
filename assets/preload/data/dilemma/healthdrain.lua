bcee46063fc312d1d63ebcc3a75a71122f614518
Justudude321 <105064710+Justudude321@users.noreply.github.com>
2022-08-13 21:19:15 -0400

    adjustment, still be 2 songs and 2 different tutorials until I can start making sprites

---
+++ assets/preload/data/dilemma/healthdrain.lua
@@ -0,0 +1,7 @@
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+
+	if dadName == 'garDT' then
+	setProperty('health', getProperty('health') - 0.011);
+	end
+	
+end
---
+++ assets/preload/data/parish/healthdrain.lua
@@ -0,0 +1,7 @@
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+
+	if dadName == 'sarvente' then
+	setProperty('health', getProperty('health') - 0.011);
+	end
+	
+end
